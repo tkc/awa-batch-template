d63a80bfd08b220cf8147fa73d581b54fe91730b
tkc <tkc@users.noreply.github.com>
2025-04-04 08:38:37 +0900

    fix : update packege name

--- example/infra/batch/src/run_batch.py
+++ example/infra/batch/src/run_batch.py
@@ -4,9 +4,9 @@ import sys
 
 # Import explicitly from the installed 'awa_batch_processor' package
 # Assuming the package structure allows importing from 'awa_batch_processor.src'
-from awa-batch-processor.src.main import sample1  # type: ignore
-from awa-batch-processor.src.models import Sample1Params  # type: ignore
-from awa-batch-processor.src.config import load_config_from_env # type: ignore
+from awa_batch_processor.src.main import sample1  # type: ignore
+from awa_batch_processor.src.models import Sample1Params  # type: ignore
+from awa_batch_processor.src.config import load_config_from_env # type: ignore
 
 
 def main():
